83900dd696870d73567951e6e184bf9fa7e919dd
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 19:09:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def apply_discount
-    @total = @total.to_f - ((@discount.to_f / 100.0) * (@total.to_f))
+    @total.to_f - ((@discount.to_f / 100.0) * (@total.to_f))
   end
   
 end
