5a6dcb3c75666f9b911d9a5a3f35382feb209991
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 18:59:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,13 @@
 class CashRegister
   attr_accessor :total, :discount
   
-  def initialize(discount = 0)
+  def initialize(discount = nil)
     @total = 0
     @discount = discount
+  end
+  
+  def add_item(title, price)
+    @total += price 
   end
   
 end
