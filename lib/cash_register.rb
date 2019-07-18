45c1d2c94f3fd94aa049764336911ed87f69dc24
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 18:57:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,9 @@
 class CashRegister
   attr_accessor :total 
   
-  def initialize
+  def initialize(employee_discount = nil)
     @total = 0
+    @employee_discount = employee_discount
   end
   
 end
